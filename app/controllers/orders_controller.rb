4ad519e522bb635d180f19443ea0861c64b32a67
mamedaihuku21 <syou.maeshiro@gmail.com>
2025-11-03 13:50:10 +0900

    クレジットカードの情報は購入の都度入力させること

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:index, :create]
+   before_action :redirect_if_seller_or_sold, only: [:index, :create]
 
   def index
     @order_address = OrderAddress.new
@@ -9,13 +10,7 @@ class OrdersController < ApplicationController
   def create
     @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
-      Payjp.api_key = ""
-      Payjp::Charge.create(
-        amount: @item.price,
-        card: order_params[:token],
-        currency: 'jpy'
-      )
-      #次は決済処理が行われるか確認しようのところから
+      pay_item
       @order_address.save
       return redirect_to root_path
     else
@@ -34,4 +29,19 @@ class OrdersController < ApplicationController
           .merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
-end
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
+      Payjp::Charge.create(
+        amount: @item.price,
+        card: order_params[:token],
+        currency: 'jpy'
+      )
+    end
+  end  
+
+  def redirect_if_seller_or_sold
+    if @item.order.present? || current_user.id == @item.user_id
+      redirect_to root_path
+    end
+  end
+
