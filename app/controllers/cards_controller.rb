05822d8772a4ccb803d324b3952cdfa165fc6dd7
mamedaihuku21 <syou.maeshiro@gmail.com>
2025-11-05 14:28:53 +0900

    エラーハンドリングができること（入力に問題がある状態で「購入」ボタンが押された場合、情報は受け入れられず、購入ページでエラーメッセージが表示されること）。

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -9,14 +9,14 @@ class CardsController < ApplicationController
       description: 'test', 
       card: params[:token] 
     )
-    card = Card.new( # 顧客トークンとログインしているユーザーを紐付けるインスタンスを生成
-      customer_token: customer.id, # 顧客トークン
-      user_id: current_user.id # ログインしているユーザー
+    card = Card.new(
+      customer_token: customer.id,
+      user_id: current_user.id
     )
     if card.save
       redirect_to root_path
     else
-      redirect_to action: "new" # カード登録画面へリダイレクト
+      redirect_to action: "new"
     end
   end  
 
